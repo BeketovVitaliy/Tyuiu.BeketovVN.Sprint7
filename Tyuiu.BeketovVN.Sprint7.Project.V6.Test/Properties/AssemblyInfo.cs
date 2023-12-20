f4b57c9e44952387a23eda0cbc1a057fabf1a2fd
Avanor <avanor5242@yandex.ru>
2023-12-20 17:12:40 +0500

    Add project files.

---
+++ Tyuiu.BeketovVN.Sprint7.Project.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BeketovVN.Sprint7.Project.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BeketovVN.Sprint7.Project.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29692c66-d21f-4f95-8e5e-2fb514f882b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
